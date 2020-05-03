3ed8aa31c5aff7e510320eb2585e221cdb7c0f0d
19h47 <jeremylevron@19h47.fr>
2020-05-03 18:34:09 +0200

    :sparkles: Select variations

---
+++ woocommerce/single-product/add-to-cart/variation.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Single variation display
+ *
+ * @package UNI/Templates
+ */
+
+use Timber\{ Timber };
+
+Timber::render( 'woocommerce/single-product/add-to-cart/variation.html.twig' );
