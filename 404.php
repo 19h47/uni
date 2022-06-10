3af86e13f5fc07dbc76dbeef59452f68b54f49cf
19h47 <jeremylevron@19h47.fr>
2022-06-10 15:31:06 +0200

    :recycle: Refacto

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 404
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @package UNI
+ * @author  Jérémy Levron <jeremylevron@19h47.fr> (https://19h47.fr)
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ * @since 0.0.0
+ * @version 0.0.0
+ */
+
+use Timber\{ Timber, Post };
+
+
+$context = Timber::context();
+
+$context['post']      = new Post();
+$context['namespace'] = 'page';
+
+$templates = array( 'pages/404.html.twig' );
+
+Timber::render( $templates, $context );
